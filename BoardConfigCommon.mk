Harpia-development/device_motorola_msm8916-common

msm8916-common: Add BoringSSL compatibility library
Improves camera quality

# Conflicts:
#	BoardConfigCommon.mk

BoardConfigCommon.mk
@@ -133,7 +133,9 @@ TARGET_LD_SHIM_LIBS := \
     /system/lib/libmot_sensorlistener.so|libsensor.so \
     /system/lib/libjustshoot.so|libshims_camera.so \
     /system/vendor/lib/libmmcamera_wavelet_lib.so|libc_util.so \
-    /system/vendor/lib/libizat_core.so|libshims_get_process_name.so
+    /system/vendor/lib/libizat_core.so|libshims_get_process_name.so\
+    /system/vendor/lib/libmmqjpeg_codec.so|libboringssl-compat.so \
+    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true

AOSP-ROM/android_device_motorola_msm8916-common

Change-Id: I07ee35a9ac0f2f16edb1370a19a0ace268d89504

BoardConfigCommon.mk
@@ -131,7 +131,9 @@ TARGET_LD_SHIM_LIBS := \
     /system/lib/libmdmcutback.so|libqsap_shim.so \
     /system/lib/libmot_sensorlistener.so|libsensor.so \
     /system/vendor/lib/libizat_core.so|libshims_get_process_name.so \
-    /system/vendor/lib/libmmcamera_wavelet_lib.so|libc_util.so
+    /system/vendor/lib/libmmcamera_wavelet_lib.so|libc_util.so \
+    /system/vendor/lib/libmmqjpeg_codec.so|libboringssl-compat.so \
+    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true